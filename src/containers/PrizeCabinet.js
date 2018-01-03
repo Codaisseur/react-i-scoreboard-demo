64d45c8c99db677ea74e8e0256ef1b68f36ab253
Bram Koot <git@bramkoot.nl>
2018-01-03 18:19:59 +0100

    Remove mutability from trophy selection

--- src/containers/PrizeCabinet.js
+++ src/containers/PrizeCabinet.js
@@ -12,31 +12,22 @@ export default class PrizeCabinet extends PureComponent {
   }
 
   trophies() {
-    let selected = []
-    let tropies = ['gold', 'silver', 'bronze']
-    let nextTrophy = tropies.shift()
+    const tropies = ['gold', 'silver', 'bronze']
 
     return this.props.players
       .sort((p1, p2) => (p2.score - p1.score))
-      .filter((player) => {
-        const { score } = player
-
-        if (selected.length < 3 || selected[selected.length - 1] === score) {
-          selected.push(score)
-          return true
-        }
-
-        return false
-      })
-      .map((player, index, players) => {
-        const playerTrophy = { ...player, trophy: nextTrophy }
-
-        if (players[index+1] && players[index+1].score < player.score) {
-          nextTrophy = tropies.shift()
-        }
-
-        return playerTrophy
-      })
+      .reduce((grouped, player) => {
+        const el = grouped.find(vals => vals[0].score === player.score)
+        if (el) el.push(player) 
+        else grouped.push([player])
+        return grouped        
+      }, [])
+      .slice(0, 3)
+      .map((set, index) => set.map(player => ({
+        ...player,
+        trophy: tropies[index]
+      })))
+      .reduce((a, b) => a.concat(b))
   }
 
   renderTropies = () => {
